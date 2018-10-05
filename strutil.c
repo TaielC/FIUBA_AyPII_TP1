7a4e73baa5ecb49c352b8c85dff2756d2948c1de
TC <taielcolavecchia@gmial.com>
2018-10-05 08:31:00 -0300

    Creado funciones de strutil.split + strutil.c tiene firmas de funcion

---
+++ strutil.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "strutil.h"
+
+
+
+int split_identificar_cantidad( const char* cadena , char separador ){
+	int cant_separar = 0;
+	int contador = 0;
+	while( cadena[contador] != '\0' ){
+		if( cadena[contador] == separador )
+			cant_separar++;
+		contador++;
+	}
+	//En caso de que el separador sea '\0' se verá en la posición final de la cadena
+	if( cadena[contador] == separador )
+			cant_separar++;
+
+	return cant_separar;
+}
+
+int* split_identificar_posiciones( const char* cadena , char separador , int cant_separar){
+
+	int* posiciones = malloc( sizeof(int) * cant_separar );
+	int contador = 0;
+	int pos_encontradas = 0;
+	while( pos_encontradas <= cant_separar ){
+		if( cadena[contador] == separador ){
+			posiciones[pos_encontradas] = contador;
+			pos_encontradas++;
+		}
+		contador++;
+	}
+
+	return posiciones;
+}
+
+char** split( const char* cadena , char separador ){
+
+	int cant_separar = split_identificar_cantidad( cadena , separador );
+	char** separados = malloc( sizeof(char*) * cant_separar );
+
+	int* pos_separar = split_identificar_posiciones( cadena , separador , cant_separar);
+
+	for( size_t i = 0 ; i < cant_separar ; i++ ){
+		separados[i] = malloc( sizeof(char) * pos_separar[i] +1 );
+		separados[i] = strndup( &cadena[pos_separar[i]] , pos_separar[i] );
+		separados[i][pos_separar[i]+1] = '\0';
+	}
+
+	return separados;
+}
+
+char* join( const char** arr_cadenas , char separador ); 
+
+void free_strv( char** arr_cadenas );
---
+++ strutil_pruebas_alumno.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+#include "testing.h"
+#include "strutil.h"
+
